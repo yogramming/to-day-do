3167183a042684db112fe30092ee6d2875e17105
yogramming <yogeshdubey9336@gmail.com>
2025-07-17 03:10:30 +0530

    Implement NoteDetailPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import NoteDetailPage from "./pages/NoteDetailPage.jsx";
 const App = () => {
   return (
     <div className="relative h-full w-full">
+      {/* bg.ibelick.com */}
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#a991f740_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
